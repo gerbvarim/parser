87cc0d7d7fba97193662cab162a7160e4ca1ad51
zivlan <zivlan92@gmail.com>
2020-03-17 18:30:42 +0200

    basic gerber_line, with ability to self_detect_command and get variables values

---
+++ src/gerber_line.py
@@ -0,0 +1,43 @@
+class GerberLine(object):
+    """
+    an object which represent a line in a gerber file.
+    it should be able to do some basic processing on it.
+    """
+
+    def __init__(self, line_string):
+        self.line_string = line_string
+    
+    def _dec_string_to_num(self, start_index):
+        """
+        read decimal value from the line string, starting at givven index.
+        """
+        digits = ['0','1','2','3','4','5','6','7','8','9']
+        num = 0
+        index = start_index
+        while index < len(self.line_string) and self.line_string[index] in digits:
+            num = num * 10
+            num += ord(self.line_string[index]) - ord('0')
+            index += 1
+            
+        return num
+    
+    def line_is_command(self):
+        """
+        check if the line is a gerber command.
+        """
+        return (self.line_string[0] != '%' and 'D' in self.line_string)
+    
+    def get_var_dec_value(self, var):
+        """
+        return the var value of a gerber line.
+        if non-exist, return none.
+        typical var for example is 'X', 'Y', 'D'
+        """
+        if not(self.line_is_command()):
+            return None
+        var_index = self.line_string.find(var)
+        if var_index == -1:
+            return None
+        return self._dec_string_to_num(var_index + 1)
+        
+        
